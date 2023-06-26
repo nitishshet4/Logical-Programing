2dee23f1230b058b862483bc14bab188c8a78637
nitishshet4 <127190978+nitishshet4@users.noreply.github.com>
2023-06-26 15:05:38 +0530

    Update and rename P8sol.java to P08.java

--- P08.java
+++ P08.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class P8sol{
+public class P8{
   public static  void main(String args[]){
     Scanner s=new Scanner (System.in);
     System.out.println("enter the alphabet");
